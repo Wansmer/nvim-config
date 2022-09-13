bc0d1672c828c7b7efd396fa9e74aba19b46dd27
wansmer <wansmer@gmail.com>
2022-09-13 12:24:33 +0300

    add typescript serverpath to the new_config volar

--- lua/config/lsp/servers/volar.lua
+++ lua/config/lsp/servers/volar.lua
@@ -14,4 +14,8 @@ return {
   --     serverPath = '/Users/wansmer/.nvm/versions/node/v16.15.0/lib/node_modules/typescript/lib/tsserverlibrary.js',
   --   },
   -- },
+  on_new_config = function(new_config)
+    new_config.init_options.typescript.serverPath =
+      '/Users/wansmer/.nvm/versions/node/v16.15.0/lib/node_modules/typescript/lib/tsserverlibrary.js'
+  end,
 }
