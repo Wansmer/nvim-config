00f9d341fefad87bfbe3bf0e209ef6b9b3b75244
Wansmer <wansmer@gmail.com>
2022-11-14 17:00:15 +0300

    update colorizer config

--- lua/config/plugins/colorizer.lua
+++ lua/config/plugins/colorizer.lua
@@ -1,20 +1,25 @@
 local present, colorizer = pcall(require, 'colorizer')
 
 if not present then
-  print('nvim-colorizer')
   return
 end
 
-colorizer.setup(nil, {
-  RGB = true, -- #RGB hex codes
-  RRGGBB = true, -- #RRGGBB hex codes
-  names = true, -- "Name" codes like Blue
-  RRGGBBAA = true, -- #RRGGBBAA hex codes
-  rgb_fn = true, -- CSS rgb() and rgba() functions
-  hsl_fn = true, -- CSS hsl() and hsla() functions
-  css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-  css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-  -- Available modes: foreground, background
-  mode = 'virtualtext', -- Set the display mode.
-  virtualtext = '',
+colorizer.setup({
+  filetypes = { 'html', 'vue', 'css', 'scss' },
+  user_default_options = {
+    RGB = true, -- #RGB hex codes
+    RRGGBB = true, -- #RRGGBB hex codes
+    names = false, -- "Name" codes like Blue
+    RRGGBBAA = true, -- #RRGGBBAA hex codes
+    rgb_fn = true, -- CSS rgb() and rgba() functions
+    hsl_fn = true, -- CSS hsl() and hsla() functions
+    css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+    css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+    sass = {
+      enable = true,
+      parsers = { 'css' },
+    },
+    mode = 'virtualtext', -- Set the display mode.
+    virtualtext = '',
+  },
 })
