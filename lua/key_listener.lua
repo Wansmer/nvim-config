3700c4f08c6ce09f2ab088f5af2c52dda10aa4ca
Wansmer <wansmer@gmail.com>
2022-12-20 13:22:15 +0300

    Clean key_listener

--- lua/key_listener.lua
+++ lua/key_listener.lua
@@ -1,6 +1,6 @@
 local listener_ls = vim.api.nvim_create_namespace('key_listener')
 
--- Убирает подстветку hlsearch при движении курсора
+-- Deleting hlsearch when it already no needed
 local function manage_hlsearch(char)
   local keys = { '<CR>', 'n', 'N', '*', '#', '?', '/' }
   if vim.fn.mode() == 'n' then
@@ -11,44 +11,13 @@ local function manage_hlsearch(char)
   end
 end
 
--- Убирает задержку между j и k (или j и j), если они назначены на <esc>
--- NOTE: рассчитана только (!!!) на два символа, потому что больше не надо и для оптимизации
--- Проблемы: 1) ставит буфер в modified, даже без изменений, 2) на пустых строках оставляет пробел после <S-S>
-local press_esc = (function()
-  local prev_char = ''
-  local escape = PREF.common.escape_keys
-  local start = vim.fn.strcharpart(escape, 0, 1)
-  local end_ = vim.fn.strcharpart(escape, 1, 1)
-  -- Или timeoutlen
-  local delay = 350
-  local clear_prev = function()
-    prev_char = ''
-  end
-
-  return function(char)
-    if char == start then
-      prev_char = char
-      vim.defer_fn(clear_prev, delay)
-    elseif char == end_ and prev_char ~= '' then
-      --       ^^^                   ^^^
-      -- NOTE: чтобы не выполнять конкатинацию на каждый ввод
-      if prev_char .. char == escape then
-        -- TODO: добавить проверку на пустую строку
-        vim.api.nvim_input('<BS><BS><ESC>')
-        prev_char = ''
-      end
-    end
-  end
-end)()
-
+---Handler for pressing keys. Added listeners for modes
+---@param char string
 local function key_listener(char)
   local key = vim.fn.keytrans(char)
   local mode = vim.fn.mode()
   if mode == 'n' then
     manage_hlsearch(key)
-    -- elseif mode == 'i' then
-    -- NOTE: неудобств больше, чем удобств
-    -- press_esc(key)
   end
 end
 
