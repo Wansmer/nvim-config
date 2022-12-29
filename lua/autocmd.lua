1d37a6b01c5e50157051b27222d07024317c2035
Wansmer <wansmer@gmail.com>
2022-12-30 00:09:55 +0300

    Added moving to " mark when open file

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -44,6 +44,18 @@ if PREF.lsp.format_on_save then
   })
 end
 
+-- Перейти к месту в файле, на котором остановились
+vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Загрузка модулей по событию VeryLazy (Lazy.nvim)
 vim.api.nvim_create_autocmd('User', {
   pattern = 'VeryLazy',
   callback = function()
