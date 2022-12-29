12c173c8f6cabdcc3a9569ca567747a159c83eca
Wansmer <wansmer@gmail.com>
2022-12-30 00:07:46 +0300

    Updated ZenMode

--- lua/config/plugins/zenmode.lua
+++ lua/config/plugins/zenmode.lua
@@ -1,7 +1,10 @@
 return {
   'folke/zen-mode.nvim',
   cmd = 'ZenMode',
+  init = function()
+    vim.keymap.set('n', 'Z', '<Cmd>ZenMode<Cr>')
+  end,
   config = function()
-    require('zen-mode').setup({})
+    require('zen-mode').setup()
   end,
 }
