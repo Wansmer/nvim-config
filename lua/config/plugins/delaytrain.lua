5fb03a0effd4a21053fc57329e80d644e3cc83af
Wansmer <wansmer@gmail.com>
2023-01-02 21:05:49 +0300

    Added plugins for training

--- lua/config/plugins/delaytrain.lua
+++ lua/config/plugins/delaytrain.lua
@@ -1,10 +1,18 @@
 return {
-  'ja-ford/delaytrain.nvim',
+  -- NOTE: use my PR until author no fix #7
+  'Wansmer/delaytrain.nvim',
   lazy = false,
-  enabled = false,
+  enabled = true,
   config = function()
     require('delaytrain').setup({
-      ignore_filetypes = { 'neo-tree', 'alpha' },
+      ignore_filetypes = {
+        'neo-tree',
+        'alpha',
+        'toggleterm',
+        'packer',
+        'lazy',
+        'TelescopePrompt',
+      },
     })
   end,
 }
---
+++ lua/config/plugins/train.lua
@@ -0,0 +1,5 @@
+return {
+  'tjdevries/train.nvim',
+  enabled = true,
+  lazy = false,
+}
