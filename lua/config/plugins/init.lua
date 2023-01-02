97cfa36eb7a45f14feb516b4a1bd85fe4c219f8b
Wansmer <wansmer@gmail.com>
2023-01-02 21:09:41 +0300

    Added vim-be-good

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -1,6 +1,10 @@
 return {
   { 'nvim-lua/plenary.nvim' },
   {
+    'ThePrimeagen/vim-be-good',
+    cmd = 'VimBeGood',
+  },
+  {
     'nvim-tree/nvim-web-devicons',
     config = function()
       require('nvim-web-devicons').setup({ default = true })
