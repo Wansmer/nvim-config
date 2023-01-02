eca334c2a4e62defc652cfc64c89fba89205c045
Wansmer <wansmer@gmail.com>
2023-01-02 23:16:35 +0300

    Added md list handler

--- lua/config/plugins/delaytrain.lua
+++ lua/config/plugins/delaytrain.lua
@@ -2,7 +2,7 @@ return {
   -- NOTE: use my PR until author no fix #7
   'Wansmer/delaytrain.nvim',
   lazy = false,
-  enabled = true,
+  enabled = false,
   config = function()
     require('delaytrain').setup({
       ignore_filetypes = {
