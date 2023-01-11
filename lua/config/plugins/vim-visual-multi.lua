a568bd8205c3a90ef5d05b3c0525f753e20275db
Wansmer <wansmer@gmail.com>
2023-01-11 20:25:03 +0300

    Added vim-visual-multi

---
+++ lua/config/plugins/vim-visual-multi.lua
@@ -0,0 +1,10 @@
+return {
+  'mg979/vim-visual-multi',
+  enabled = true,
+  config = function ()
+    vim.g.VM_custom_remaps = {
+      -- ['<C-d>'] = '<C-Down>',
+      -- ['<C-u>'] = '<C-Up>',
+    }
+  end
+}
