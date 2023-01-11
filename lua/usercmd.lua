0577aa8c48334d3e666605cc5579a9a1993693f3
Wansmer <wansmer@gmail.com>
2023-01-11 20:22:00 +0300

    Formatted usercmd

--- lua/usercmd.lua
+++ lua/usercmd.lua
@@ -1,7 +1,3 @@
-vim.api.nvim_create_user_command(
-  'H',
-  function(args)
-    vim.cmd('vert h ' .. args.args)
-  end,
-  {nargs = 1, complete = 'help'}
-)
+vim.api.nvim_create_user_command('H', function(args)
+  vim.cmd('vert h ' .. args.args)
+end, { nargs = 1, complete = 'help' })
