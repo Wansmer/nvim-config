6322cc745bc576663b8a76c0542c482c917af6c3
Wansmer <wansmer@gmail.com>
2023-01-12 13:18:05 +0300

    Fix watcher

--- init.lua
+++ init.lua
@@ -12,20 +12,18 @@ require('config.colorscheme')
 -- TODO: перенести в отдельный модуль
 local w = vim.loop.new_fs_event()
 if w then
-  local CWD = vim.loop.cwd()
+  local CWD = vim.loop.cwd() .. '/'
 
   local Watcher = {}
 
-  local function wrapper()
-    vim.schedule_wrap(function(...)
-      Watcher.on_change(...)
-    end)
-  end
+  local wrapper = vim.schedule_wrap(function(...)
+    Watcher.on_change(...)
+  end)
 
   Watcher.on_change = function(err, fname, status)
-    vim.api.nvim_command('checktime')
+    vim.cmd.checktime()
     w:stop()
-    Watcher.watch_file(CWD)
+    Watcher.watch_file(CWD, wrapper)
   end
 
   Watcher.watch_file = function(path, cb)
