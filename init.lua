2dd4fd26409896cf6aa650d4f2c74a06083b284c
Wansmer <wansmer@gmail.com>
2023-01-12 12:33:21 +0300

    Added watcher for directory

--- init.lua
+++ init.lua
@@ -8,3 +8,29 @@ require('plugins')
 require('mappings')
 require('autocmd')
 require('config.colorscheme')
+
+-- TODO: перенести в отдельный модуль
+local w = vim.loop.new_fs_event()
+if w then
+  local CWD = vim.loop.cwd()
+
+  local Watcher = {}
+
+  local function wrapper()
+    vim.schedule_wrap(function(...)
+      Watcher.on_change(...)
+    end)
+  end
+
+  Watcher.on_change = function(err, fname, status)
+    vim.api.nvim_command('checktime')
+    w:stop()
+    Watcher.watch_file(CWD)
+  end
+
+  Watcher.watch_file = function(path, cb)
+    w:start(path, {}, cb)
+  end
+
+  Watcher.watch_file(CWD, wrapper)
+end
