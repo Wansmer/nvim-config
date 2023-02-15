d760cd142bbd3dc2c18f56ca38f2fdd14b6dc207
Wansmer <wansmer@gmail.com>
2023-02-15 15:46:32 +0300

    Added Gr command

--- lua/usercmd.lua
+++ lua/usercmd.lua
@@ -27,3 +27,19 @@ vim.api.nvim_create_user_command('DiffOrig', function()
     end, { buffer = buf })
   end
 end, {})
+
+vim.api.nvim_create_user_command('Gr', function(args)
+  local start = vim.api.nvim_get_current_buf()
+  local ft = vim.api.nvim_get_option_value('filetype', { buf = start })
+  vim.cmd('vnew | set buftype=nofile | read ++edit # | set filetype=' .. ft .. ' | 0d_ | g!/' .. args.args .. '/d')
+
+  local scratch = vim.api.nvim_get_current_buf()
+
+  -- Map `q` for both buffers to exit diff view and delete scratch buffer
+  for _, buf in ipairs({ scratch, start }) do
+    vim.keymap.set('n', 'q', function()
+      vim.api.nvim_buf_delete(scratch, { force = true })
+      vim.keymap.del('n', 'q', { buffer = start })
+    end, { buffer = buf })
+  end
+end, { nargs = 1 })
