5af81254c70012919464c406e2d2d8ca4c5ace23
Wansmer <wansmer@gmail.com>
2023-04-08 00:57:58 +0300

    Add comment plugins

---
+++ lua/config/plugins/comment.lua
@@ -0,0 +1,10 @@
+return {
+  'numToStr/Comment.nvim',
+  event = { 'BufReadPost' },
+  dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
+  config = function()
+    require('Comment').setup({
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    })
+  end,
+}
---
+++ lua/config/plugins/ts-context-commentstring.lua
@@ -0,0 +1,6 @@
+return {
+  'JoosepAlviste/nvim-ts-context-commentstring',
+  event = 'BufReadPost',
+  enabled = true,
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+}
