ba2b59bb3da331b80ef87ad0e2d64d93c32e6a64
Wansmer <wansmer@gmail.com>
2023-04-19 00:20:52 +0300

    Global undercurl for diag

--- lua/config/colorscheme/init.lua
+++ lua/config/colorscheme/init.lua
@@ -20,3 +20,9 @@ local present, _ = pcall(vim.cmd.colorscheme, colorscheme)
 if not present then
   vim.cmd.colorscheme('habamax')
 end
+
+for _, type in pairs({ 'Error', 'Warn', 'Hint', 'Info' }) do
+  local hl = 'DiagnosticUnderline' .. type
+  local colors = vim.api.nvim_get_hl(0, { name = hl })
+  vim.api.nvim_set_hl(0, hl, vim.tbl_extend('force', colors, { undercurl = true }))
+end
