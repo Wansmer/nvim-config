8af9fa3416ba0f8b2c6967117632db8484d4781a
Wansmer <wansmer@gmail.com>
2023-05-02 12:44:06 +0300

    Update Zenmode spec

--- lua/config/plugins/zenmode.lua
+++ lua/config/plugins/zenmode.lua
@@ -1,7 +1,23 @@
 return {
   'folke/zen-mode.nvim',
-  keys = { { 'Z', ':ZenMode<CR>' } },
+  keys = {
+    { '<leader>z', ':ZenMode<CR>' },
+  },
   config = function()
-    require('zen-mode').setup()
+    require('zen-mode').setup({
+      on_open = function(win)
+        local ft = vim.api.nvim_buf_get_option(0, 'ft')
+        vim.api.nvim_win_set_width(win, vim.api.nvim_buf_get_option(0, 'tw'))
+
+        -- For better writing
+        local text = { 'markdown', 'text' }
+        if vim.tbl_contains(text, ft) then
+          local opts = { tw = 0, linebreak = true, wrap = true }
+          for opt, val in pairs(opts) do
+            vim.opt_local[opt] = val
+          end
+        end
+      end,
+    })
   end,
 }
