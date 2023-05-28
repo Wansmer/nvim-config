687a2f0c61719f2e7a5b93b6e9c1f5d2b6b5f75a
Wansmer <wansmer@gmail.com>
2023-05-28 09:50:22 +0300

    Tune monokai

--- lua/config/colorscheme/monokai.lua
+++ lua/config/colorscheme/monokai.lua
@@ -3,7 +3,7 @@ require('monokai-pro').setup({
   terminal_colors = true,
   devicons = true, -- highlight the icons of `nvim-web-devicons`
   styles = {
-    comment = { italic = true },
+    comment = { italic = PREF.ui.italic_comment },
     keyword = { italic = true }, -- any other keyword
     type = { italic = true }, -- (preferred) int, long, char, etc
     storageclass = { italic = true }, -- static, register, volatile, etc
@@ -16,27 +16,22 @@ require('monokai-pro').setup({
   -- Enable this will disable filter option
   day_night = {
     enable = false, -- turn off by default
-    day_filter = 'classic', -- classic | octagon | pro | machine | ristretto | spectrum
-    night_filter = 'ristretto', -- classic | octagon | pro | machine | ristretto | spectrum
+    day_filter = 'octagon', -- classic | octagon | pro | machine | ristretto | spectrum
+    night_filter = 'octagon', -- classic | octagon | pro | machine | ristretto | spectrum
   },
   inc_search = 'background', -- underline | background
-  background_clear = {
-    -- "float_win",
-    'toggleterm',
-    'telescope',
-    'which-key',
-    'renamer',
-  }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree"
+  background_clear = {}, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree"
   plugins = {
     bufferline = {
       underline_selected = false,
       underline_visible = false,
     },
-    indent_blankline = {
-      context_highlight = 'default', -- default | pro
-      context_start_underline = false,
-    },
+    -- indent_blankline = {
+    --   context_highlight = 'default', -- default | pro
+    --   context_start_underline = false,
+    -- },
   },
   ---@param c Colorscheme
+  ---@diagnostic disable-next-line: unused-local
   override = function(c) end,
 })
