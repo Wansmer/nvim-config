1903e41ed0d2085ef5778fed43815ce7425bdc70
Wansmer <wansmer@gmail.com>
2023-06-16 15:48:44 +0300

    Fix non-start comment.nvim with `nvim ./file` command

--- lua/config/plugins/comment.lua
+++ lua/config/plugins/comment.lua
@@ -1,6 +1,6 @@
 return {
   'numToStr/Comment.nvim',
-  event = { 'BufReadPost' },
+  event = { 'BufEnter' },
   dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
   config = function()
     require('Comment').setup({
