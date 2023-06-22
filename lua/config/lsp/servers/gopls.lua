b678938300e5b230d6d76dd8120b3990569cefd8
Wansmer <wansmer@gmail.com>
2023-06-22 08:27:27 +0300

    add settings for gopls

---
+++ lua/config/lsp/servers/gopls.lua
@@ -0,0 +1,16 @@
+return {
+  settings = {
+    gopls = {
+      hints = {
+        -- https://github.com/golang/tools/blob/master/gopls/doc/inlayHints.md
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+    },
+  },
+}
