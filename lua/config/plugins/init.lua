59b4229e83d63109b0c9782d2879eb1b545ee200
Wansmer <wansmer@gmail.com>
2023-08-11 13:00:24 +0300

    add mini.splitjoin

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -1,4 +1,10 @@
 return {
   { 'nvim-lua/plenary.nvim' },
   { 'nvim-tree/nvim-web-devicons' },
+  {
+    'echasnovski/mini.splitjoin',
+    config = function()
+      require('mini.splitjoin').setup()
+    end,
+  },
 }
