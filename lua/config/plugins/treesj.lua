dbc27e3180cdff7798a89ba7d1df3fc059481a94
Wansmer <wansmer@gmail.com>
2023-08-11 13:00:44 +0300

    add fallback for comments in treesj

--- lua/config/plugins/treesj.lua
+++ lua/config/plugins/treesj.lua
@@ -6,10 +6,25 @@ return {
   dev = DEV,
   keys = { '<Leader>m', '<Leader>M' },
   enabled = true,
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
   config = function()
-    require('treesj').setup({
-      max_join_length = 1000,
-    })
+    local langs = require('treesj.langs').presets
+
+    for _, nodes in pairs(langs) do
+      nodes.comment = {
+        both = {
+          fallback = function(tsn)
+            local res = require('mini.splitjoin').toggle()
+            if not res then
+              vim.cmd('normal! gww')
+            end
+          end,
+        },
+      }
+    end
+
+    require('treesj').setup({ max_join_length = 1000 })
+
     vim.keymap.set('n', '<Leader>M', function()
       require('treesj').toggle({ split = { recursive = true }, join = { recursive = true } })
     end, { desc = 'Toggle single/multiline block of code' })
