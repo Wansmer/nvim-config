3ff277b27362e64882585580fda744da824137e2
Wansmer <wansmer@gmail.com>
2023-08-27 10:05:51 +0300

    add clear map for codeium

--- lua/config/plugins/codeium.lua
+++ lua/config/plugins/codeium.lua
@@ -1,6 +1,7 @@
 return {
   'Exafunction/codeium.vim',
   event = 'BufEnter',
+  enabled = true,
   config = function()
     vim.g.codeium_disable_bindings = 1
     vim.keymap.set('i', '<C-g>', function()
@@ -9,12 +10,15 @@ return {
     vim.keymap.set('i', '<C-j>', function()
       return vim.fn['codeium#Complete']()
     end, { expr = true })
-    vim.keymap.set('i', '<c-i>', function()
+    vim.keymap.set('i', '<C-i>', function()
       return vim.fn['codeium#CycleCompletions'](1)
     end, { expr = true })
     vim.keymap.set('i', '<c-o>', function()
       return vim.fn['codeium#CycleCompletions'](-1)
     end, { expr = true })
-    -- vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+    -- TODO: <C-x> conflict with cmp, find best map
+    vim.keymap.set('i', '<C-x>', function()
+      return vim.fn['codeium#Clear']()
+    end, { expr = true })
   end,
 }
