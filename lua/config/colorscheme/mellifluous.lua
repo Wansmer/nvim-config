bc4132135cc0f1229b0ad98938a146ed68a6d4c8
Wansmer <wansmer@gmail.com>
2023-09-07 20:41:03 +0300

    update mellifluous preset

--- lua/config/colorscheme/mellifluous.lua
+++ lua/config/colorscheme/mellifluous.lua
@@ -1,6 +1,6 @@
 require('mellifluous').setup({
-  dim_inactive = true,
-  color_set = 'mellifluous',
+  dim_inactive = false,
+  color_set = 'mellifluous', -- 'mellifluous', 'alduin', 'tender', 'mountain'
   styles = { -- see :h attr-list for options. set {} for NONE, { option = true } for option
     comments = { italic = true },
     conditionals = {},
@@ -18,17 +18,17 @@ require('mellifluous').setup({
   },
   transparent_background = {
     enabled = false,
-    floating_windows = true,
-    telescope = true,
-    file_tree = true,
-    cursor_line = true,
+    floating_windows = false,
+    telescope = false,
+    file_tree = false,
+    cursor_line = false,
     status_line = false,
   },
   flat_background = {
-    line_numbers = false,
+    line_numbers = true,
     floating_windows = false,
     file_tree = false,
-    cursor_line_number = false,
+    cursor_line_number = true,
   },
   plugins = {
     cmp = true,
@@ -37,6 +37,9 @@ require('mellifluous').setup({
     nvim_tree = {
       enabled = true,
       show_root = false,
+    },
+    neo_tree = {
+      enabled = true,
     },
     telescope = {
       enabled = true,
