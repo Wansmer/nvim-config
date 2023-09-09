59bdae0d89d85ad15a6af476340eaf8cf039212a
Wansmer <wansmer@gmail.com>
2023-09-10 00:18:06 +0300

    updae ill spec

--- lua/config/plugins/illuminate.lua
+++ lua/config/plugins/illuminate.lua
@@ -7,8 +7,5 @@ return {
       filetypes_denylist = { 'alpha', 'neo-tree', 'toggleterm' },
       min_count_to_highlight = 2,
     })
-    vim.api.nvim_set_hl(0, 'IlluminatedWordText', { link = 'Visual' })
-    vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { link = 'Visual' })
-    vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { link = 'Visual' })
   end,
 }
