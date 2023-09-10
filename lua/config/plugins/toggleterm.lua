a4b6d2f6d70094b134c0adf5833115acc4c670ab
Wansmer <wansmer@gmail.com>
2023-09-11 00:11:55 +0300

    make toggleterm float

--- lua/config/plugins/toggleterm.lua
+++ lua/config/plugins/toggleterm.lua
@@ -11,21 +11,40 @@ return {
       open_mapping = [[<C-;>]],
       hide_numbers = true,
       shade_filetypes = {},
-      shade_terminals = true,
-      shading_factor = 3,
       start_in_insert = true,
       insert_mappings = true,
-      persist_size = true,
+      persist_size = false,
       persist_mode = false,
-      direction = 'horizontal',
+      direction = 'float', -- 'float', 'horizontal', 'vertical', 'tab'
       close_on_exit = true,
       shell = vim.o.shell,
+      highlights = {
+        Normal = { link = 'NeoTreeNorml' },
+      },
+      float_opts = {
+        border = { '', '', '', ' ', ' ', ' ', ' ', ' ' },
+        relative = 'editor',
+        width = vim.opt.columns:get(),
+        height = 20,
+        col = 0,
+        row = vim.opt.lines:get(),
+        style = 'minimal',
+        title = 'Hello KITTY',
+        title_pos = 'left',
+        noautocmd = true,
+      },
+      on_open = function(term)
+        local ns = vim.api.nvim_create_namespace('ToggleTerm')
+        vim.api.nvim_set_hl(ns, 'Normal', { link = 'TTNormal' })
+        vim.api.nvim_set_hl(ns, 'FloatBorder', { link = 'TTBorder' })
+        vim.api.nvim_win_set_hl_ns(term.window, ns)
+      end,
     })
 
     vim.api.nvim_create_autocmd('filetype', {
       pattern = 'toggleterm',
       callback = function()
-        vim.wo.statuscolumn = '    '
+        -- vim.wo.statuscolumn = '    '
         vim.wo.cursorline = false
       end,
     })
