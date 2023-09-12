4a507eb17acb4642a8276b9c8e84d7f1421002ba
Wansmer <wansmer@gmail.com>
2023-09-12 14:29:41 +0300

    moved lsp-depended autocmd to lsp folder

---
+++ lua/config/lsp/autocmd.lua
@@ -0,0 +1,30 @@
+--[[ LSP dependent autocommands ]]
+
+if vim.fn.has('nvim-0.10.0') then
+  vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'Enable inlayHint feature',
+    callback = function(args)
+      local bufnr = args.buf
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if client and client.supports_method('textDocument/inlayHint', { bufnr = bufnr }) then
+        vim.lsp.inlay_hint(bufnr, PREF.lsp.show_inlay_hints)
+      end
+    end,
+  })
+end
+
+-- {{ Toggle diagnostic dependent of insert mode
+vim.api.nvim_create_autocmd('InsertEnter', {
+  desc = 'Hide diagnostic messages in insert mode',
+  callback = function()
+    vim.diagnostic.disable()
+  end,
+})
+
+vim.api.nvim_create_autocmd('InsertLeave', {
+  desc = 'Show diagnostic messages in normal mode',
+  callback = function()
+    vim.diagnostic.enable()
+  end,
+})
+-- }}
