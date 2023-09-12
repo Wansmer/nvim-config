d6ceaf917c4b6f7d90aa19d5624e58b338ca1b04
Wansmer <wansmer@gmail.com>
2023-09-12 14:29:20 +0300

    disable `missing-fields` feature by lua_ls

--- lua/config/lsp/servers/lua_ls.lua
+++ lua/config/lsp/servers/lua_ls.lua
@@ -1,4 +1,6 @@
 -- https://github.com/luals/lua-language-server
+-- https://github.com/LuaLS/lua-language-server/blob/16b9ce9bafbf0f432ab7d1d063e2f18b1ed0c947/doc/en-us/config.md
+
 local settings = {
   Lua = {
     hint = {
@@ -7,6 +9,7 @@ local settings = {
     },
     diagnostics = {
       globals = { 'vim', 'USER_SETTINGS' },
+      disable = { 'missing-fields' },
     },
     workspace = {
       library = {
