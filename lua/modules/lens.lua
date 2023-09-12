c577babd8c670973509d2df5138aad2748ba44cf
Wansmer <wansmer@gmail.com>
2023-09-12 23:59:39 +0300

    update lens

--- lua/modules/lens.lua
+++ lua/modules/lens.lua
@@ -8,6 +8,9 @@ local SymbolKind = vim.lsp.protocol.SymbolKind
 local u = require('utils')
 local ns = vim.api.nvim_create_namespace('__lens__')
 
+local SYMBOLS = 'textDocument/documentSymbol'
+local REFERENCES = 'textDocument/references'
+
 -- Full list: https://github.com/neovim/neovim/blob/1f551e068f728ff38bd7fdcfa3a6daf362bab9da/runtime/lua/vim/lsp/protocol.lua#L119
 local target_kinds = {
   SymbolKind.Function,
@@ -15,11 +18,14 @@ local target_kinds = {
   SymbolKind.Class,
   SymbolKind.Interface,
   SymbolKind.Struct,
+  -- SymbolKind.Variable,
 }
 
-local function supports_method(method, bufnr)
+local function supports_methods(bufnr)
   for _, client in ipairs(vim.lsp.get_clients({ bufnr = bufnr })) do
-    if client.supports_method(method, { bufnr = bufnr }) then
+    local is_symbols = client.supports_method(SYMBOLS, { bufnr = bufnr })
+    local is_references = client.supports_method(REFERENCES, { bufnr = bufnr })
+    if is_symbols and is_references then
       return true
     end
   end
@@ -29,36 +35,39 @@ end
 local function show_usage(bufnr, id, res)
   local ln = res.range.start.line
 
-  vim.api.nvim_buf_set_extmark(bufnr, ns, ln, 0, {
-    id = id,
-    virt_text = { { ' ' .. res.references .. ' usage', 'Comment' } },
-    virt_text_pos = 'eol',
-    hl_mode = 'combine',
-  })
+  if res.references ~= 0 then
+    vim.api.nvim_buf_set_extmark(bufnr, ns, ln, 0, {
+      id = id,
+      virt_text = { { ' ' .. res.references .. ' usage', 'Comment' } },
+      virt_text_pos = 'eol',
+      hl_mode = 'combine',
+    })
+  end
 end
 
 local function make_params(ref)
   return {
     context = { includeDeclaration = false },
     position = ref.selectionRange.start,
-    textDocument = { uri = vim.uri_from_bufnr(0) }, }
+    textDocument = { uri = vim.uri_from_bufnr(0) },
+  }
 end
 
 local function collect_references(symbols, bufnr)
   for idx, sym in ipairs(symbols) do
-    local method = 'textDocument/references'
-
     local function handler(response)
       for _, resp in ipairs(response) do
         if not resp.error and resp.result then
-          show_usage(bufnr, idx, { name = sym.name, references = #resp.result, range = sym.range })
+          show_usage(bufnr, idx, {
+            name = sym.name,
+            references = #resp.result,
+            range = sym.range,
+          })
         end
       end
     end
 
-    if supports_method(method, bufnr) then
-      vim.lsp.buf_request_all(bufnr, method, sym.params, handler)
-    end
+    vim.lsp.buf_request_all(bufnr, REFERENCES, sym.params, handler)
   end
 end
 
@@ -79,7 +88,7 @@ local function filter_by_kinds(dst, symbols)
 end
 
 local function collect_document_symbols(bufnr)
-  local method = 'textDocument/documentSymbol'
+  -- local method = 'textDocument/documentSymbol'
   local symbols = {}
 
   local function handler(response)
@@ -92,9 +101,9 @@ local function collect_document_symbols(bufnr)
     collect_references(symbols, bufnr)
   end
 
-  if supports_method(method, bufnr) then
+  if supports_methods(bufnr) then
     local params = { textDocument = vim.lsp.util.make_text_document_params() }
-    vim.lsp.buf_request_all(bufnr, method, params, handler)
+    vim.lsp.buf_request_all(bufnr, SYMBOLS, params, handler)
   end
 end
 
