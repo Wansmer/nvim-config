2f7ac1c840004c7e41909da9b9aec96a54066e46
Wansmer <wansmer@gmail.com>
2023-09-12 23:19:16 +0300

    update lens

--- lua/modules/lens.lua
+++ lua/modules/lens.lua
@@ -1,6 +1,9 @@
 -- Shows usage count for functions, methods, class, structs, and interface on the same line
 -- Knowleges: https://github.com/VidocqH/lsp-lens.nvim/blob/main/lua/lsp-lens/init.lua
 
+-- TODO:
+-- [ ] ignore anonimus func
+
 local SymbolKind = vim.lsp.protocol.SymbolKind
 local u = require('utils')
 local ns = vim.api.nvim_create_namespace('__lens__')
@@ -25,14 +28,11 @@ end
 
 local function show_usage(bufnr, id, res)
   local ln = res.range.start.line
-  local text = vim.api.nvim_buf_get_lines(bufnr, ln, ln + 1, true)[1]
-  local len = vim.fn.strchars(text)
 
   vim.api.nvim_buf_set_extmark(bufnr, ns, ln, 0, {
     id = id,
-    virt_text = { { res.references .. ' usage', 'Comment' } },
-    virt_text_pos = 'overlay',
-    virt_text_win_col = len + 2,
+    virt_text = { { ' ' .. res.references .. ' usage', 'Comment' } },
+    virt_text_pos = 'eol',
     hl_mode = 'combine',
   })
 end
@@ -41,8 +41,7 @@ local function make_params(ref)
   return {
     context = { includeDeclaration = false },
     position = ref.selectionRange.start,
-    textDocument = { uri = vim.uri_from_bufnr(0) },
-  }
+    textDocument = { uri = vim.uri_from_bufnr(0) }, }
 end
 
 local function collect_references(symbols, bufnr)
