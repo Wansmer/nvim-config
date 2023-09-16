10d1d14cf4a931be1c21d0e43a5656a1ef91cb86
Wansmer <wansmer@gmail.com>
2023-09-16 22:38:21 +0300

    update symbol hl

--- lua/config/plugins/symbol-usage.lua
+++ lua/config/plugins/symbol-usage.lua
@@ -7,7 +7,7 @@ return {
     local ok, c = pcall(require, 'serenity.colors')
     local hl = { link = 'Comment' }
     if ok then
-      hl = { fg = c.cursor_line_number, bold = false }
+      hl = { fg = c.cursor_line_number, bold = false, italic = true }
     end
     require('symbol-usage').setup({
       hl = hl,
