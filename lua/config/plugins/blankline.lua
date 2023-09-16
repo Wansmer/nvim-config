cfbef6df8b1647d4fa64ce0ceebf3266389afd9a
Wansmer <wansmer@gmail.com>
2023-09-16 21:01:25 +0300

    update indent-blankline spec

--- lua/config/plugins/blankline.lua
+++ lua/config/plugins/blankline.lua
@@ -1,12 +1,16 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
   event = 'BufEnter',
-  init = function ()
+  init = function()
+    local c = require('serenity.colors')
     vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { link = 'NeoTreeIndentMarker' })
+    vim.api.nvim_set_hl(0, 'IndentBlanklineContextChar', { fg = c.accent })
   end,
   confit = function()
     require('indent_blankline').setup({
+      -- show_current_context = true,
       show_current_context = true,
+      show_current_context_start = true,
     })
   end,
 }
