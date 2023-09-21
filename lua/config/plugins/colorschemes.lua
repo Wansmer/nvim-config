c0f5424e436ba550b542aa9254bb75dcb1ca1365
Wansmer <wansmer@gmail.com>
2023-09-21 13:34:11 +0300

    changed colorschemes load behavior

--- lua/config/plugins/colorschemes.lua
+++ lua/config/plugins/colorschemes.lua
@@ -1,69 +1,20 @@
-return {
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    enabled = PREF.ui.colorscheme == 'tokyonight',
-  },
-  {
-    'catppuccin/nvim',
-    lazy = false,
-    name = 'catppuccin',
-    build = ':CatppuccinCompile',
-    enabled = PREF.ui.colorscheme == 'catppuccin',
-  },
-  {
-    'sam4llis/nvim-tundra',
-    lazy = false,
-    enabled = PREF.ui.colorscheme == 'tundra',
-  },
-  {
-    'rebelot/kanagawa.nvim',
-    lazy = false,
-    enabled = PREF.ui.colorscheme == 'kanagawa',
-  },
-  {
-    'Wansmer/serenity.nvim',
-    dev = true,
-    dir = '~/projects/code/personal/serenity.nvim',
-    lazy = false,
-    enabled = PREF.ui.colorscheme == 'serenity',
-  },
-  {
-    'sainnhe/gruvbox-material',
-    lazy = false,
-    enabled = PREF.ui.colorscheme == 'gruvbox-material',
-  },
-  {
-    'Mofiqul/vscode.nvim',
-    lazy = false,
-    enabled = PREF.ui.colorscheme == 'vscode',
-  },
-  {
-    'neanias/everforest-nvim',
-    version = false,
-    lazy = false,
-    enabled = PREF.ui.colorscheme == 'everforest',
-  },
-  {
-    'ramojus/mellifluous.nvim',
-    dependencies = { 'rktjmp/lush.nvim' },
-    lazy = false,
-    enabled = PREF.ui.colorscheme == 'mellifluous',
-  },
-  {
-    'loctvl842/monokai-pro.nvim',
-    lazy = false,
-    enabled = PREF.ui.colorscheme == 'monokai-pro',
-  },
-  {
-    'dasupradyumna/midnight.nvim',
-    lazy = false,
-    enabled = PREF.ui.colorscheme == 'midnight',
-  },
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    lazy = false,
-    enabled = PREF.ui.colorscheme == 'rose-pine',
-  },
-}
+local function set_prior(c)
+  c = type(c) == 'table' and c or { c }
+  c.priority = 1000
+  return c
+end
+
+return vim.tbl_map(set_prior, {
+  'Mofiqul/vscode.nvim',
+  'dasupradyumna/midnight.nvim',
+  'folke/tokyonight.nvim',
+  'loctvl842/monokai-pro.nvim',
+  'rebelot/kanagawa.nvim',
+  'sainnhe/gruvbox-material',
+  'sam4llis/nvim-tundra',
+  { 'Wansmer/serenity.nvim', dev = true, dir = '~/projects/code/personal/serenity.nvim' },
+  { 'catppuccin/nvim', name = 'catppuccin' },
+  { 'neanias/everforest-nvim', version = false },
+  { 'ramojus/mellifluous.nvim', dependencies = { 'rktjmp/lush.nvim' } },
+  { 'rose-pine/neovim', name = 'rose-pine' },
+})
