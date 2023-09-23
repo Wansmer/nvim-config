54d316ea094bfea09017c7c306e13fe2fec1533c
Wansmer <wansmer@gmail.com>
2023-09-23 09:48:06 +0300

    update illuminate spec

--- lua/config/plugins/illuminate.lua
+++ lua/config/plugins/illuminate.lua
@@ -4,7 +4,7 @@ return {
   enabled = true,
   config = function()
     require('illuminate').configure({
-      filetypes_denylist = { 'alpha', 'neo-tree', 'toggleterm' },
+      filetypes_denylist = { 'alpha', 'neo-tree', 'toggleterm', 'aerial' },
       min_count_to_highlight = 2,
     })
   end,
