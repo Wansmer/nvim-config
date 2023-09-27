cde9c7833c5a951e3c9f2c156e7402d98a4988ee
Wansmer <wansmer@gmail.com>
2023-09-27 17:49:44 +0300

    update foldtext

--- lua/modules/foldtext.lua
+++ lua/modules/foldtext.lua
@@ -1,18 +1,19 @@
 ---@module Foldtext
 ---Based on https://www.reddit.com/r/neovim/comments/16sqyjz/finally_we_can_have_highlighted_folds/
-function HighlightedFoldtext()
-  local pos = vim.v.foldstart
+
+local function parse_line(pos)
   local line = vim.api.nvim_buf_get_lines(0, pos - 1, pos, false)[1]
+
   local ok, parser = pcall(vim.treesitter.get_parser, 0)
   -- If treesitter is not attached to the buffer
   if not ok then
-    return vim.fn.foldtext()
+    return nil
   end
 
   local query = vim.treesitter.query.get(parser:lang(), 'highlights')
 
-  if query == nil then
-    return vim.fn.foldtext()
+  if not query then
+    return nil
   end
 
   local tree = parser:parse({ pos - 1, pos })[1]
@@ -46,10 +47,48 @@ function HighlightedFoldtext()
       prev_range = range
     end
   end
+  return result
+end
 
-  table.insert(result, { ' { +' .. vim.v.foldend - vim.v.foldstart .. ' lines }', 'Folded' })
+function HighlightedFoldtext()
+  local result = parse_line(vim.v.foldstart)
+  if not result then
+    return vim.fn.foldtext()
+  end
+
+  local folded = {
+    { ' ', 'Folded' },
+    { '+' .. vim.v.foldend - vim.v.foldstart .. ' lines', 'CursorLine' },
+    { ' ', 'Folded' },
+  }
+
+  for _, item in ipairs(folded) do
+    table.insert(result, item)
+  end
+
+  local result2 = parse_line(vim.v.foldend)
+  if result2 then
+    local first = result2[1]
+    result2[1] = { vim.trim(first[1]), first[2] }
+    for _, item in ipairs(result2) do
+      table.insert(result, item)
+    end
+  end
 
   return result
 end
+
+local function set_fold_hl()
+  local fg = vim.api.nvim_get_hl(0, { name = 'CursorLineNr' }).fg
+  local hl = vim.api.nvim_get_hl(0, { name = 'Folded' })
+  hl.fg = fg
+  vim.api.nvim_set_hl(0, 'Folded', hl)
+end
+
+set_fold_hl()
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = set_fold_hl,
+})
 
 return 'luaeval("HighlightedFoldtext")()'
