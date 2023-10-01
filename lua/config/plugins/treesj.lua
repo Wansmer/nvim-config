40fa769fe1efefffbde807de83d204ef5a8e2889
Wansmer <wansmer@gmail.com>
2023-10-02 01:59:01 +0300

    update treesj spec

--- lua/config/plugins/treesj.lua
+++ lua/config/plugins/treesj.lua
@@ -4,7 +4,7 @@ return {
   'Wansmer/treesj',
   dir = DEV and '~/projects/code/personal/treesj' or nil,
   dev = DEV,
-  keys = { '<Leader>m', '<Leader>M' },
+  keys = { '<Leader>m', '<Leader>M', '<leader>s', '<leader>j' },
   enabled = true,
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   config = function()
@@ -25,15 +25,18 @@ return {
 
     require('treesj').setup({
       max_join_length = 1000,
-      use_default_keymaps = false,
+      use_default_keymaps = true,
     })
 
     vim.keymap.set('n', '<Leader>M', function()
       require('treesj').toggle({ split = { recursive = true }, join = { recursive = true } })
     end, { desc = 'Toggle single/multiline block of code' })
+
     vim.keymap.set('n', '<Leader>m', function()
       local tsj_langs = require('treesj.langs')['presets']
-      if tsj_langs[vim.bo.filetype] then
+      local ok, _ = pcall(vim.treesitter.get_parser, 0, vim.bo.filetype)
+
+      if tsj_langs[vim.bo.filetype] and ok then
         require('treesj').toggle()
       else
         require('mini.splitjoin').toggle()
