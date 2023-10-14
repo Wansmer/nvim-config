e36803c62f5ee4ecfb9df17f6642692d654f131d
Wansmer <wansmer@gmail.com>
2023-10-14 16:36:32 +0300

    update `mini.pick` spec

--- lua/config/plugins/pick.lua
+++ lua/config/plugins/pick.lua
@@ -4,31 +4,49 @@ return {
   enabled = true,
   version = false,
   config = function()
-    local pick = require('mini.pick')
+    local MiniPick = require('mini.pick')
 
-    pick.registry.grep_live_current_buf = function(opts)
-      local lines = vim.api.nvim_buf_get_lines(0, 1, -1, false)
-      local items = {}
+    MiniPick.registry.grep_live_current_buf = function(local_opts)
+      -- Parse options
+      local_opts = vim.tbl_deep_extend('force', { buf_id = nil, prompt = '' }, local_opts or {})
+      local buf_id, prompt = local_opts.buf_id, local_opts.prompt
+      local_opts.buf_id, local_opts.prompt = nil, nil
 
-      for lnum, line in ipairs(lines) do
-        items[lnum] = { text = line, lnum = lnum + 1, path = vim.fn.expand('%p') }
+      -- Construct items
+      if buf_id == nil or buf_id == 0 then
+        buf_id = vim.api.nvim_get_current_buf()
       end
 
-      pick.start({
-        source = {
-          items = items,
-          name = 'Grep current buffer',
-        },
-      })
+      local lines = vim.api.nvim_buf_get_lines(buf_id, 0, -1, false)
+      local items = {}
+      for i, l in ipairs(lines) do
+        items[i] = { text = l, bufnr = buf_id, lnum = i }
+      end
+
+      -- Start picker while scheduling setting the query
+      vim.schedule(function()
+        MiniPick.set_picker_query(vim.split(prompt, ''))
+      end)
+      MiniPick.start({ source = { items = items, name = 'Grep live current buf' } })
     end
 
     local map = vim.keymap.set
-    map('n', '<localleader>f', pick.builtin.files, { desc = 'Pick: Find files in (cwd>' })
-    map('n', '<localleader>g', pick.builtin.grep_live, { desc = 'Pick: live grep (cwd>' })
-    map('n', '<localleader>b', pick.builtin.buffers, { desc = 'Pick: show open buffers' })
-    map('n', '<localleader>s', pick.registry.grep_live_current_buf, { desc = 'Pick: live grep current buf' })
+    map('n', '<localleader>s', function()
+      MiniPick.registry.grep_live_current_buf({ prompt = vim.fn.expand('<cword>') })
+    end, { desc = 'Pick: live grep current buf <cword>' })
 
-    pick.setup({
+    map('n', '<localleader>S', function()
+      MiniPick.builtin.grep(
+        { pattern = '<cword>' },
+        { source = { name = 'Grep (rg) <cword>: ' .. vim.fn.expand('<cword>') } }
+      )
+    end, { desc = 'Pick: live grep <cword> (cwd)' })
+
+    map('n', '<localleader>f', MiniPick.builtin.files, { desc = 'Pick: Find files in (cwd)' })
+    map('n', '<localleader>g', MiniPick.builtin.grep_live, { desc = 'Pick: live grep (cwd)' })
+    map('n', '<localleader>b', MiniPick.builtin.buffers, { desc = 'Pick: show open buffers' })
+
+    MiniPick.setup({
       options = {
         content_from_bottom = true,
         use_cache = false,
