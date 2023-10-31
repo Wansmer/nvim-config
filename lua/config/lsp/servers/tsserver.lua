229a2e5dec0d6bf6f7447310b0b47ac875b5f540
Wansmer <wansmer@gmail.com>
2023-10-31 15:20:57 +0300

    update tsserver spec

--- lua/config/lsp/servers/tsserver.lua
+++ lua/config/lsp/servers/tsserver.lua
@@ -1,5 +1,5 @@
 return {
-  autostart = not PREF.lsp.tom_enable,
+  -- autostart = not PREF.lsp.tom_enable,
   -- https://www.reddit.com/r/neovim/comments/14e41rb/comment/jou4ljw/?utm_source=share&utm_medium=web2x&context=3
   -- settings = {
   --   typescript = {
