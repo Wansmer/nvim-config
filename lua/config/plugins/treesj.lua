5a5ddfb119a72b2edc018ca11f4f89a06cfc0c6b
Wansmer <wansmer@gmail.com>
2023-12-02 14:45:52 +0300

    fix treest-mini.splitjoin fallback

--- lua/config/plugins/treesj.lua
+++ lua/config/plugins/treesj.lua
@@ -32,11 +32,23 @@ return {
       require('treesj').toggle({ split = { recursive = true }, join = { recursive = true } })
     end, { desc = 'Toggle single/multiline block of code' })
 
+    local function get_pos_lang(node)
+      local c = vim.api.nvim_win_get_cursor(0)
+      local range = { c[1] - 1, c[2], c[1] - 1, c[2] }
+      local buf = vim.api.nvim_get_current_buf()
+      local ok, parser = pcall(vim.treesitter.get_parser, buf, vim.treesitter.language.get_lang(vim.bo[buf].ft))
+      if not ok then
+        return ''
+      end
+      local current_tree = parser:language_for_range(range)
+      return current_tree:lang()
+    end
+
     vim.keymap.set('n', '<Leader>m', function()
       local tsj_langs = require('treesj.langs')['presets']
-      local ok, _ = pcall(vim.treesitter.get_parser, 0, vim.bo.filetype)
-
-      if tsj_langs[vim.bo.filetype] and ok then
+      local lang = get_pos_lang()
+      print('Lang: ' .. lang)
+      if lang ~= '' and tsj_langs[lang] then
         require('treesj').toggle()
       else
         require('mini.splitjoin').toggle()
