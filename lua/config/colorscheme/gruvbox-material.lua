f0e9734b67398d43989c07ac894f8b5e024fb425
Wansmer <wansmer@gmail.com>
2023-12-09 18:43:16 +0300

    tune gruvbox

--- lua/config/colorscheme/gruvbox-material.lua
+++ lua/config/colorscheme/gruvbox-material.lua
@@ -1,6 +1,6 @@
 vim.g.gruvbox_material_background = 'hard' -- 'hard', 'medium', 'soft'
 vim.g.gruvbox_material_disable_italic_comment = not PREF.ui.italic_comment
 vim.g.gruvbox_material_enable_bold = true
-vim.g.gruvbox_material_enable_italic = true
+vim.g.gruvbox_material_enable_italic = false
 vim.g.gruvbox_material_cursor = 'auto'
 vim.g.gruvbox_material_transparent_background = 0
