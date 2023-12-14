293baa8ac9a15d2775cc26e4c98f74c089c799b7
Wansmer <wansmer@gmail.com>
2023-12-14 17:56:08 +0300

    tune illuminate

--- lua/config/plugins/illuminate.lua
+++ lua/config/plugins/illuminate.lua
@@ -7,5 +7,11 @@ return {
       filetypes_denylist = { 'alpha', 'neo-tree', 'toggleterm', 'aerial' },
       min_count_to_highlight = 2,
     })
+
+    if vim.g.colors_name ~= 'serenity' then
+      vim.api.nvim_set_hl(0, 'IlluminatedWordText', { link = 'Visual' })
+      vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { link = 'Visual' })
+      vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { link = 'Visual' })
+    end
   end,
 }
