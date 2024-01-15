4d6d2b52e8b0b33793949ae56a8dc026b0969282
Wansmer <wansmer@gmail.com>
2024-01-15 13:08:59 +0300

    uncomment tsserver settings for inlay_hints

--- lua/config/lsp/servers/tsserver.lua
+++ lua/config/lsp/servers/tsserver.lua
@@ -1,30 +1,30 @@
 return {
   -- autostart = not PREF.lsp.tom_enable,
   -- https://www.reddit.com/r/neovim/comments/14e41rb/comment/jou4ljw/?utm_source=share&utm_medium=web2x&context=3
-  -- settings = {
-  --   typescript = {
-  --     inlayHints = {
-  --       includeInlayParameterNameHints = 'all',
-  --       includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-  --       includeInlayFunctionParameterTypeHints = true,
-  --       includeInlayVariableTypeHints = true,
-  --       includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-  --       includeInlayPropertyDeclarationTypeHints = true,
-  --       includeInlayFunctionLikeReturnTypeHints = true,
-  --       includeInlayEnumMemberValueHints = true,
-  --     },
-  --   },
-  --   javascript = {
-  --     inlayHints = {
-  --       includeInlayParameterNameHints = 'all',
-  --       includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-  --       includeInlayFunctionParameterTypeHints = true,
-  --       includeInlayVariableTypeHints = true,
-  --       includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-  --       includeInlayPropertyDeclarationTypeHints = true,
-  --       includeInlayFunctionLikeReturnTypeHints = true,
-  --       includeInlayEnumMemberValueHints = true,
-  --     },
-  --   },
-  -- },
+  settings = {
+    typescript = {
+      inlayHints = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+    javascript = {
+      inlayHints = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
+  },
 }
