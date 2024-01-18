33fc0c543bded5731c21182489d111fffc8a043b
Wansmer <wansmer@gmail.com>
2024-01-18 11:49:23 +0300

    update tsserver config

--- lua/config/lsp/servers/tsserver.lua
+++ lua/config/lsp/servers/tsserver.lua
@@ -1,7 +1,21 @@
+-- https://github.com/typescript-language-server/typescript-language-server/
+-- See docs/configuration.md
 return {
   -- autostart = not PREF.lsp.tom_enable,
-  -- https://www.reddit.com/r/neovim/comments/14e41rb/comment/jou4ljw/?utm_source=share&utm_medium=web2x&context=3
+  init_options = {
+    hostInfo = "neovim",
+    locale = "en",
+    preferences = {
+      providePrefixAndSuffixTextForRename = false,
+      allowRenameOfImportPath = false,
+    },
+  },
   settings = {
+    diagnostics = {
+      --@type nuber[]
+      ignoredCodes = {},
+    },
+    -- https://www.reddit.com/r/neovim/comments/14e41rb/comment/jou4ljw/?utm_source=share&utm_medium=web2x&context=3
     typescript = {
       inlayHints = {
         includeInlayParameterNameHints = "all",
