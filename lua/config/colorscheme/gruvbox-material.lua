9d247d8d59b0b20c65f97294d8a17d11a554db08
Wansmer <wansmer@gmail.com>
2024-01-25 08:26:51 +0300

    update gruvbox spec

--- lua/config/colorscheme/gruvbox-material.lua
+++ lua/config/colorscheme/gruvbox-material.lua
@@ -4,3 +4,8 @@ vim.g.gruvbox_material_enable_bold = true
 vim.g.gruvbox_material_enable_italic = false
 vim.g.gruvbox_material_cursor = "auto"
 vim.g.gruvbox_material_transparent_background = 0
+
+vim.schedule(function()
+  vim.api.nvim_set_hl(0, "WinBar", { link = "Normal" })
+  vim.api.nvim_set_hl(0, "WinBarNC", { link = "Normal" })
+end)
