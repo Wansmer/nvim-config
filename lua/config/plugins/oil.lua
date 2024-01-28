21402c4fe60bafd35421626c652ff4a9c384028c
Wansmer <wansmer@gmail.com>
2024-01-28 18:55:38 +0300

    install `oil.nvim`

---
+++ lua/config/plugins/oil.lua
@@ -0,0 +1,29 @@
+return {
+  "stevearc/oil.nvim",
+  enabled = true,
+  cmd = "Oil",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-v>"] = "actions.select_vsplit",
+        ["<C-g>"] = "actions.select_split",
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
+      -- Set to false to disable all of the above keymaps
+      use_default_keymaps = true,
+    })
+  end,
+}
