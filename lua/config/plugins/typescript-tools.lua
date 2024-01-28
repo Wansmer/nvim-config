2bbe48f48cd47745133ec807ddd4a594fcbe3a5b
Wansmer <wansmer@gmail.com>
2024-01-28 18:50:40 +0300

    install `typescript-tools`

---
+++ lua/config/plugins/typescript-tools.lua
@@ -0,0 +1,22 @@
+return {
+  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  ft = { "typescript", "typescriptreact", "typescript.tsx" },
+  config = function()
+    local on_attach = require("config.lsp.default").on_attach
+    require("typescript-tools").setup({
+      on_attach = on_attach,
+      settings = {
+        tsserver_file_preferences = {
+          includeInlayParameterNameHints = "all",
+          includeCompletionsForModuleExports = true,
+          quotePreference = "auto",
+        },
+        tsserver_format_options = {
+          allowIncompleteCompletions = false,
+          allowRenameOfImportPath = false,
+        },
+      },
+    })
+  end,
+}
