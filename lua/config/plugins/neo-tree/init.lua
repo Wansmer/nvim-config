ffec97d3679a3b301f67f6dd9db529afda7611d3
Wansmer <wansmer@gmail.com>
2024-01-28 18:51:38 +0300

    remove `triptych.nvim`

--- lua/config/plugins/neo-tree/init.lua
+++ lua/config/plugins/neo-tree/init.lua
@@ -3,8 +3,8 @@ return {
   enabled = true,
   cmd = "Neotree",
   init = function()
-    -- vim.keymap.set("n", "<LocalLeader>e", "<Cmd>Neotree focus toggle<Cr>", { desc = "Open file explorer" })
-    -- vim.keymap.set("x", "<LocalLeader>e", "<Esc><Cmd>Neotree focus toggle<Cr>", { desc = "Open file explorer" })
+    vim.keymap.set("n", "<LocalLeader>e", "<Cmd>Neotree focus toggle<Cr>", { desc = "Open file explorer" })
+    vim.keymap.set("x", "<LocalLeader>e", "<Esc><Cmd>Neotree focus toggle<Cr>", { desc = "Open file explorer" })
   end,
   dependencies = {
     "nvim-lua/plenary.nvim",
