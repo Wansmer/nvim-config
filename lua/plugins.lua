d8bf3461c7319b629c6777e8ec0bd4014c15bdb5
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-02-04 08:10:33 +0300

    update lazy download script

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,7 +6,6 @@ if not vim.loop.fs_stat(lazypath) then
     "git",
     "clone",
     "--filter=blob:none",
-    "--single-branch",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
     lazypath,
