6e875f6b8f252866f3e581d768f8a38dfd0bcadb
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-02-17 11:02:15 +0300

    update `bufferline` spec

--- lua/config/plugins/bufferline.lua
+++ lua/config/plugins/bufferline.lua
@@ -4,10 +4,16 @@ return {
   version = "*",
   event = "VeryLazy",
   config = function()
+    vim.api.nvim_win_get_position(0)
+    vim.api.nvim_win_get_config(0)
+    vim.api.nvim_tabpage_list_wins(0)
     require("bufferline").setup({
       options = {
         diagnostics = false,
-        offsets = { { filetype = "neo-tree", text = "File Explorer" } },
+        offsets = {
+          { filetype = "neo-tree", text = "File Explorer" },
+          { filetype = "aerial", text = "Document Symbols" },
+        },
       },
     })
   end,
