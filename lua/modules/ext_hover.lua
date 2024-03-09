825c44c10f88331b4c566a1b92d7c78986db9e87
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-03-09 11:10:12 +0300

    refactor ext_hover

--- lua/modules/ext_hover.lua
+++ lua/modules/ext_hover.lua
@@ -1,18 +1,14 @@
 ---Implements extended hover functionality
 ---(e.g. vscode like `editor.action.showDefinitionPreviewHover`)
----
----Requirements:
---- - `sed`;
-
-local M = { servers = { "tsserver" } }
 
 local p = vim.lsp.protocol.Methods
+local M = { servers = { "tsserver" } }
 
 function M.extended_hover()
   local start_buf = vim.api.nvim_get_current_buf()
 
   local clients = vim.lsp.get_clients({ bufnr = start_buf })
-  local client ---@type lsp.Client
+  local client ---@type vim.lsp.Client
 
   for _, c in ipairs(clients) do
     if
@@ -34,10 +30,19 @@ function M.extended_hover()
 
     vim.schedule(function()
       local content = vim.tbl_flatten({ lines.hover, lines.definition })
-      local max = math.max(unpack(vim.iter(content):map(string.len):totable()))
+      local max_width = math.floor(vim.o.columns * 0.4)
+      local longest_line = math.max(unpack(vim.iter(content):map(string.len):totable()))
+      if longest_line > max_width then
+        longest_line = max_width
+      end
 
-      table.insert(content, #lines.hover + 1, (""):rep(max))
-      vim.lsp.util.open_floating_preview(content, "markdown", { focus_id = "hover" })
+      table.insert(content, #lines.hover + 1, (""):rep(longest_line))
+      vim.lsp.util.open_floating_preview(content, "markdown", {
+        border = PREF.ui.border,
+        max_height = math.floor(vim.o.lines * 0.5),
+        max_width = max_width,
+        focus_id = "ext_hover",
+      })
     end)
   end)
 
@@ -55,6 +60,49 @@ function M.extended_hover()
     handle_result({ hover = content })
   end, start_buf)
 
+  ---Get certain lines from a file
+  ---@param path string
+  ---@param startline integer
+  ---@param endline integer
+  ---@return string[]
+  local function get_lines(path, startline, endline)
+    local file = io.open(path, "r")
+    if not file then
+      return {}
+    end
+    local lines = vim.iter(file:lines()):skip(startline - 1):take(endline - (startline - 1)):totable()
+    file:close()
+    return lines
+  end
+
+  ---Trim indent of each line according to the first line
+  ---@param lines string[]
+  ---@return string[]
+  local function trim_indent(lines)
+    local indent = lines[1]:match("^(%s*)")
+    for i, line in ipairs(lines) do
+      if vim.startswith(line, indent) then
+        lines[i] = line:sub(#indent + 1)
+      end
+    end
+    return lines
+  end
+
+  ---Wrap the lines in a markdown code block. If lines is empty, return an empty table
+  ---@param path string
+  ---@param lines string[]
+  ---@return string[]
+  local function wrap_md(path, lines)
+    if vim.tbl_isempty(lines) then
+      return lines
+    end
+    local ft = vim.filetype.match({ filename = path })
+    if not ft or ft == "" then
+      ft = vim.fn.fnamemodify(path, ":e")
+    end
+    return vim.tbl_flatten({ "```" .. ft, lines, "```" })
+  end
+
   -- DEFINITION
   client.request(p.textDocument_definition, params, function(err, result, ctx, config)
     if err or not result or vim.tbl_isempty(result) or ctx.bufnr ~= start_buf then
@@ -68,22 +116,12 @@ function M.extended_hover()
     local path = vim.uri_to_fname(def.targetUri)
     local start_line = def.targetRange.start.line + 1
     local end_line = def.targetRange["end"].line + 1
-    local cmd = { "sed", "-n", start_line .. "," .. end_line .. " p", path }
-    local on_exit = vim.schedule_wrap(function(res)
-      if res.code ~= 0 then
-        vim.notify("Problem in `sed` in ext_hover", vim.log.levels.INFO)
-        return
-      end
-      handle_result({
-        definition = vim.tbl_flatten({
-          "```" .. vim.fn.expand("%:e"),
-          vim.split(res.stdout, "\n", { trimempty = true }),
-          "```",
-        }),
-      })
-    end)
+    local lines = get_lines(path, start_line, end_line)
+    lines = trim_indent(lines)
 
-    vim.system(cmd, { text = true }, on_exit)
+    handle_result({
+      definition = wrap_md(path, lines),
+    })
   end, start_buf)
 end
 
