f81af48e359c02d7b2090d11ef4d4940d651d345
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-05-10 13:58:23 +0300

    feat: install `render-markdown`

---
+++ lua/config/plugins/md-renderer.lua
@@ -0,0 +1,9 @@
+return {
+  "MeanderingProgrammer/markdown.nvim",
+  name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  ft = { "markdown" },
+  config = function()
+    require("render-markdown").setup({})
+  end,
+}
