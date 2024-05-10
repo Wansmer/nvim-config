37f1d8de82dc65e56d5e349cb5c85b466bc459ac
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-05-10 14:44:25 +0300

    chore: start `md-renderer` with lazy=false

--- lua/config/plugins/md-renderer.lua
+++ lua/config/plugins/md-renderer.lua
@@ -2,7 +2,8 @@ return {
   "MeanderingProgrammer/markdown.nvim",
   name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  ft = { "markdown" },
+  -- ft = { "markdown" },
+  lazy = false,
   config = function()
     require("render-markdown").setup({})
   end,
