124f54ea2ea3f5f4b0d9180314f2169acea3370a
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-05-19 13:21:26 +0300

    feat: dedefined gruvbox hls

--- lua/config/colorscheme/gruvbox-material.lua
+++ lua/config/colorscheme/gruvbox-material.lua
@@ -6,6 +6,30 @@ vim.g.gruvbox_material_cursor = "auto"
 vim.g.gruvbox_material_transparent_background = 0
 
 vim.schedule(function()
-  vim.api.nvim_set_hl(0, "WinBar", { link = "Normal" })
-  vim.api.nvim_set_hl(0, "WinBarNC", { link = "Normal" })
+  local h = vim.api.nvim_set_hl
+  local get_hl = function(name)
+    return vim.api.nvim_get_hl(0, { name = name })
+  end
+  h(0, "WinBar", { link = "Normal" })
+  h(0, "WinBarNC", { link = "Normal" })
+
+  local nnc = get_hl("NeoTreeNormal")
+  h(0, "NormalFloat", { link = "NeoTreeNormal" })
+  h(0, "FloatBorder", { bg = nnc.bg, fg = nnc.bg })
+  h(0, "NeoTreeVertSplit", { bg = nnc.bg, fg = nnc.bg, force = true }) -- TODO: Why not works?
+
+  -- {{ Telescope
+  h(0, "TelescopePromptBorder", { link = "FloatBorder" })
+  h(0, "TelescopePromptNormal", { link = "FloatBorder" })
+  h(0, "TelescopePromptTitle", { bg = get_hl("Orange").fg, bold = true })
+
+  h(0, "TelescopePreviewBorder", { link = "FloatBorder" })
+  h(0, "TelescopePreviewNormal", { bg = get_hl("NeoTreeVertSplit").bg })
+  h(0, "TelescopePreviewTitle", { bg = get_hl("Green").fg, bold = true })
+
+  local cc = get_hl("ColorColumn")
+  h(0, "TelescopeResultsBorder", { bg = cc.bg, fg = cc.bg })
+  h(0, "TelescopeResultsNormal", { bg = cc.bg })
+  h(0, "TelescopeResultsTitle", { bg = get_hl("Blue").fg, bold = true })
+  -- }}
 end)
