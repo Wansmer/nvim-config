40a74a5d1c30feea213ba95331904a63025f866f
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-05-23 13:08:42 +0300

    feat: add hack for `getcharstr`

--- init.lua
+++ init.lua
@@ -4,6 +4,16 @@ vim.keymap.set("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "["
 
+local orig_getcharstr = vim.fn.getcharstr
+vim.fn.getcharstr = function() ---@diagnostic disable-line: duplicate-set-field
+  local char = orig_getcharstr()
+  local ok, lm = pcall(require, "langmapper.utils")
+  if not ok then
+    return char
+  end
+  return lm.translate_keycode(char, "default", "ru")
+end
+
 require("user_settings")
 require("options")
 require("plugins")
