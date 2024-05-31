c3dbdd1efd97256bc2d3d87bfa7ef202c1c907c0
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-05-31 10:44:43 +0300

    refactor (lsp): check legacy config in `esling` lsp

--- lua/config/lsp/servers/eslint.lua
+++ lua/config/lsp/servers/eslint.lua
@@ -9,17 +9,62 @@ end
 
 ---Check if cwd contains config.eslint.js (new flat config)
 ---@return boolean
-local function is_cwd_use_flat_config()
-  return is_exist_in_cwd("config.eslint.js")
+local function is_flat_config_in_cwd()
+  return vim
+    .iter({
+      "eslint.config.js",
+      "eslint.config.mjs",
+      "eslint.config.cjs",
+      "eslint.config.ts",
+      "eslint.config.mts",
+      "eslint.config.cts",
+    })
+    :any(is_exist_in_cwd)
 end
 
----Check if flat config is used in cwd. Otherwise the 'ESLINT_USE_FLAT_CONFIG' value is used.
----If global config is used, make sure 'ESLINT_USE_FLAT_CONFIG' is set according  to the global
----config type.
+---Check if cwd contains legacy config.
+---@return boolean
+local function is_legacy_config_in_cwd()
+  print(vim
+    .iter({
+      ".eslintrc",
+      ".eslintrc.js",
+      ".eslintrc.cjs",
+      ".eslintrc.yaml",
+      ".eslintrc.yml",
+      ".eslintrc.json",
+    })
+    :any(is_exist_in_cwd))
+  return vim
+    .iter({
+      ".eslintrc",
+      ".eslintrc.js",
+      ".eslintrc.cjs",
+      ".eslintrc.yaml",
+      ".eslintrc.yml",
+      ".eslintrc.json",
+    })
+    :any(is_exist_in_cwd)
+end
+
+---Check if flat config is used in cwd. If cwd contains legacy config, reuturned false. If not
+---config detected, the 'ESLINT_USE_FLAT_CONFIG' value is used (for global config).
 ---@return boolean
 local function is_use_flat_config()
-  return is_cwd_use_flat_config() or u.to_bool(vim.env.ESLINT_USE_FLAT_CONFIG)
+  if is_legacy_config_in_cwd() then
+    return false
+  end
+  return is_flat_config_in_cwd() or u.to_bool(vim.env.ESLINT_USE_FLAT_CONFIG)
 end
+
+print(
+  "ESLINT_USE_FLAT_CONFIG",
+  is_use_flat_config(),
+  "is flat:",
+  is_flat_config_in_cwd(),
+  "Is legacy:",
+  is_legacy_config_in_cwd()
+)
 
 return {
   settings = {
