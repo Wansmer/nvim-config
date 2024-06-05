30d54c289dac9ecb4f7c918286cdd6e6df1afd27
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-06-05 08:12:56 +0300

    feat: detect ansible.yaml filetype

---
+++ lua/filetype.lua
@@ -0,0 +1,18 @@
+--{{ Detect yaml.ansible
+local function ansible_or_yaml()
+  local ansible_root = { "ansible.cfg", ".ansible-lint.yml", "inventory.ini", "inventory.yml" }
+  return vim.fs.root(vim.env.PWD, ansible_root) and "yaml.ansible" or "yaml"
+end
+
+vim.filetype.add({
+  pattern = {
+    [".*/tasks/.*.ya?ml"] = "yaml.ansible",
+    [".*/playbooks/.*.ya?ml"] = "yaml.ansible",
+    ["*playbook*.ya?ml"] = "yaml.ansible",
+  },
+  extension = {
+    ["yml"] = ansible_or_yaml(),
+    ["yaml"] = ansible_or_yaml(),
+  },
+})
+--}}
