ff7e81fb3077578b0fcec4d3a230bd08903ef872
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-06-11 10:35:03 +0300

    feat: use `trouble` instead of default qfixlist

--- lua/config/plugins/trouble.lua
+++ lua/config/plugins/trouble.lua
@@ -5,5 +5,18 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("trouble").setup({})
+
+    vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+      callback = function()
+        vim.cmd([[Trouble qflist open]])
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "qf",
+      callback = vim.schedule_wrap(function(e)
+        vim.api.nvim_buf_delete(e.buf, { force = true })
+      end),
+    })
   end,
 }
