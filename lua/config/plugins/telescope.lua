dbf01f15b57cf135c3a0c8e6918d438151b7498d
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-06-30 11:24:29 +0300

    fix: <C-m> recognized as <Cr>, change to <C-j>

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -105,7 +105,7 @@ return {
               ["<C-k>"] = lga_actions.quote_prompt(),
               ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
               -- freeze the current list and start a fuzzy search in the frozen list
-              ["<C-m>"] = actions.to_fuzzy_refine,
+              ["<C-j>"] = actions.to_fuzzy_refine,
             },
           },
         },
