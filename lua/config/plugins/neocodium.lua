27bccb2c88a4e5647f71cdce30ab38cd2a7d0703
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-07-25 13:54:43 +0300

    feat: change <C-p/n> mappings value

--- lua/config/plugins/neocodium.lua
+++ lua/config/plugins/neocodium.lua
@@ -8,11 +8,11 @@ return {
     })
     local map = vim.keymap.set
     map("i", "<C-g>", neocodeium.accept)
-    map("i", "<C-i>", function()
-      require("neocodeium").cycle_or_complete(1)
+    map("i", "<C-.>", function()
+      neocodeium.cycle_or_complete(1)
     end)
-    map("i", "<C-o>", function()
-      require("neocodeium").cycle_or_complete(-1)
+    map("i", "<C-,>", function()
+      neocodeium.cycle_or_complete(-1)
     end)
   end,
 }
