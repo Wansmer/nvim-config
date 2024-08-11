53b973d8c207a8b6ee3fc4fbf777c2f9e2575000
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-08-11 09:55:39 +0300

    chore: use `vtsls` instead of `typescript-tools`

--- lua/config/plugins/typescript-tools.lua
+++ lua/config/plugins/typescript-tools.lua
@@ -1,6 +1,6 @@
 return {
   "pmizio/typescript-tools.nvim",
-  enabled = true,
+  enabled = false,
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
   ft = {
     "javascript",
