21a740230eb9b1098bf8a3ee0db08e5c0e12fc55
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-08-14 07:50:42 +0300

    feat: tune zen-mode for writing

--- lua/config/plugins/zenmode.lua
+++ lua/config/plugins/zenmode.lua
@@ -2,20 +2,34 @@ return {
   "folke/zen-mode.nvim",
   enabled = true,
   keys = {
-    { "<leader>z", ":ZenMode<CR>" },
+    {
+      "<leader>z",
+      function()
+        require("zen-mode").toggle({
+          window = {
+            width = vim.api.nvim_get_option_value("tw", { buf = 0 }),
+          },
+        })
+      end,
+    },
   },
   config = function()
     require("zen-mode").setup({
       on_open = function(win)
         local ft = vim.api.nvim_buf_get_option(0, "ft")
-        vim.api.nvim_win_set_width(win, vim.api.nvim_buf_get_option(0, "tw"))
-
         -- For better writing
-        local text = { "markdown", "text" }
-        if vim.tbl_contains(text, ft) then
-          local opts = { tw = 0, linebreak = true, wrap = true }
-          for opt, val in pairs(opts) do
+        if vim.tbl_contains({ "markdown", "text" }, ft) then
+          for opt, val in pairs({ tw = 9999, linebreak = true, wrap = true }) do
             vim.opt_local[opt] = val
+          end
+        end
+      end,
+      on_close = function()
+        local ft = vim.api.nvim_get_option_value("ft", { buf = 0 })
+        -- restore original options
+        if vim.tbl_contains({ "markdown", "text" }, ft) then
+          for _, opt in ipairs({ "tw", "linebreak", "wrap" }) do
+            vim.opt_local[opt] = vim.filetype.get_option(ft, opt)
           end
         end
       end,
