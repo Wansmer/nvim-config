6d7cbc38a7e445b759ed62686968542cd281d3a3
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-08-14 07:51:19 +0300

    feat: use `md-renderer` instead of `markview`

--- lua/config/plugins/markview.lua
+++ lua/config/plugins/markview.lua
@@ -1,6 +1,6 @@
 return {
   "OXY2DEV/markview.nvim",
-  enabled = true,
+  enabled = false,
   lazy = false,
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
--- lua/config/plugins/md-renderer.lua
+++ lua/config/plugins/md-renderer.lua
@@ -1,6 +1,6 @@
 return {
   "MeanderingProgrammer/markdown.nvim",
-  enabled = false,
+  enabled = true,
   name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   -- ft = { "markdown" },
