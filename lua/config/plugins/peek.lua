79760649c6e6e16fe30bb3d89cea606b00a23c5d
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-08-17 18:42:03 +0300

    feat: add `peek.nvim`

---
+++ lua/config/plugins/peek.lua
@@ -0,0 +1,10 @@
+return {
+  "toppair/peek.nvim",
+  event = { "VeryLazy" },
+  build = "deno task --quiet build:fast",
+  config = function()
+    require("peek").setup()
+    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+  end,
+}
