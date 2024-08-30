d4a417d2474ef0593fc9470bae281f9b97e137c2
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-08-30 19:29:51 +0300

    feat: use neocodeium own fork

--- lua/config/plugins/neocodium.lua
+++ lua/config/plugins/neocodium.lua
@@ -1,12 +1,18 @@
+local DEV = true
 return {
-  "monkoose/neocodeium",
+  "Wansmer/neocodeium",
+  enabled = true,
+  dir = DEV and "~/projects/code/personal/neocodeium" or nil,
+  dev = DEV,
   event = "VeryLazy",
   config = function()
     local neocodeium = require("neocodeium")
     neocodeium.setup({
       silent = true,
     })
+
     local map = vim.keymap.set
+    
     map("i", "<C-g>", neocodeium.accept)
     map("i", "<C-.>", function()
       neocodeium.cycle_or_complete(1)
