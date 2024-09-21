246fdde9097994f520e89c19fb22ea646106fb17
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-09-21 20:54:11 +0300

    feat: disable return types on vtsls

--- lua/config/lsp/servers/vtsls.lua
+++ lua/config/lsp/servers/vtsls.lua
@@ -6,7 +6,7 @@ return {
         parameterTypes = { enabled = false },
         variableTypes = { enabled = false },
         propertyDeclarationTypes = { enabled = true },
-        functionLikeReturnTypes = { enabled = true },
+        functionLikeReturnTypes = { enabled = false },
         enumMemberValues = { enabled = true },
       },
     },
