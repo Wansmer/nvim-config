dd706448fbab16d2910c5d6c3718ef13b585c299
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-09-27 17:19:15 +0300

    feat: add default keymaps to restore session

--- lua/config/plugins/persistence.lua
+++ lua/config/plugins/persistence.lua
@@ -1,6 +1,15 @@
 return {
   "folke/persistence.nvim",
   event = "BufReadPre",
+  keys = {
+    {
+      "<leader>S",
+      function()
+        require("persistence").load()
+      end,
+      desc = "Restore Session",
+    },
+  },
   enabled = true,
   config = function()
     require("persistence").setup({})
