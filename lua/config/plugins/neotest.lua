8afa35fb7a32b4c9d67bcdca6ab13e3fd2fa9570
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-10-01 09:45:10 +0300

    feat: add mappings for neotest

--- lua/config/plugins/neotest.lua
+++ lua/config/plugins/neotest.lua
@@ -8,6 +8,18 @@ return {
     "nvim-treesitter/nvim-treesitter",
     "marilari88/neotest-vitest",
   },
+  init = function()
+    local map = vim.keymap.set
+    map("n", "<leader>nt", function()
+      require("neotest").run.run()
+    end, { desc = "Run nearest test" })
+    map("n", "<leader>nf", function()
+      require("neotest").run.run(vim.fn.expand("%"))
+    end, { desc = "Run current file" })
+    map("n", "<leader>no", function()
+      require("neotest").output.open({ enter = true })
+    end, { desc = "Open test output window" })
+  end,
   config = function()
     require("neotest").setup({
       adapters = {
