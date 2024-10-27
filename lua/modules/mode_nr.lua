30102b1db34dd6214c3bf87f1b74b258857ce99a
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-10-27 14:48:44 +0300

    refactor (line_nr): use `winhl` instead of set win's namespace

--- lua/modules/mode_nr.lua
+++ lua/modules/mode_nr.lua
@@ -1,6 +1,5 @@
 local PARTS_NAME = "__parts.mode_clnr__"
 local GR = vim.api.nvim_create_augroup(PARTS_NAME, { clear = true })
-local NS = vim.api.nvim_create_namespace(PARTS_NAME)
 
 local M = {}
 
@@ -13,25 +12,6 @@ function M.get_fg_from_hl(name)
   return { fg = fg, bg = bg, bold = true }
 end
 
-function M.setup(opts)
-  M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
-  vim.api.nvim_create_autocmd("ModeChanged", {
-    group = GR,
-    callback = function()
-      if not (vim.wo.number or vim.wo.relativenumber) then
-        return
-      end
-      local win = vim.api.nvim_get_current_win()
-      local mode = vim.fn.strtrans(vim.fn.mode()):lower():gsub("%W", "")
-      local override = M.opts.hls[mode] or M.opts.hls.n
-      vim.api.nvim_set_hl(NS, "CursorLineNr", override)
-      -- WARNING: if any plugins already set their namespace to this window, it will be overwritten with global namespace values
-      -- TODO: to find the PR that causing this weird behavior and open issue
-      vim.api.nvim_win_set_hl_ns(win, NS)
-    end,
-  })
-end
-
 M.opts = {
   hls = {
     ["n"] = M.get_fg_from_hl("CursorLineNr"),
@@ -40,5 +20,43 @@ M.opts = {
     ["r"] = M.get_fg_from_hl("Error"),
   },
 }
+
+function M.setup(opts)
+  M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
+
+  local mode_hl = "ModeCursorLine"
+  local alias_pair = "CursorLineNr:" .. mode_hl
+
+  vim.api.nvim_create_autocmd("WinLeave", {
+    group = GR,
+    callback = function()
+      -- Do not change the color of the line number in an inactive window
+      vim.wo.winhl = vim
+        .iter(vim.split(vim.wo.winhl, ","))
+        :filter(function(pair)
+          return pair ~= alias_pair
+        end)
+        :join(",")
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("ModeChanged", {
+    group = GR,
+    callback = function()
+      if not (vim.wo.number or vim.wo.relativenumber) then
+        return
+      end
+
+      if not vim.wo.winhl:match(mode_hl) then
+        vim.wo.winhl = (vim.wo.winhl == "" and "" or vim.wo.winhl .. ",") .. alias_pair
+      end
+
+      local mode = vim.fn.strtrans(vim.fn.mode()):lower():gsub("%W", "")
+      local override = M.opts.hls[mode] or M.opts.hls.n
+
+      vim.api.nvim_set_hl(0, mode_hl, override)
+    end,
+  })
+end
 
 return M
