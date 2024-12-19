d6c80e6d1ebd4d615888e9b8ef95131db586d9ea
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2024-12-19 18:15:49 +0300

    fix: md-preview error on update

--- lua/config/plugins/mdpreview.lua
+++ lua/config/plugins/mdpreview.lua
@@ -1,7 +1,7 @@
 return {
   "iamcco/markdown-preview.nvim",
   enabled = true,
-  build = "cd app && yarn install",
+  build = "cd app && npm install && git restore .",
   ft = { "markdown" },
   config = function()
     vim.g.mkdp_filetypes = { "markdown" }
