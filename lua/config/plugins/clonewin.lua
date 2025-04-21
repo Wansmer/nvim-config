9c9b874cd3e4549c0bd10f69b5a077464146b183
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2025-04-21 10:46:44 +0300

    feat: `clonewin` spec

---
+++ lua/config/plugins/clonewin.lua
@@ -0,0 +1,18 @@
+return {
+  "Wansmer/clonewin.nvim",
+  enabled = true,
+  lazy = false,
+  dev = true,
+  dir = "~/projects/code/personal/clonewin",
+  config = function()
+    require("clonewin.init").setup({
+      mappings = {
+        ["<C-h>"] = "<C-h>",
+        ["<C-j>"] = "<C-j>",
+        ["<C-k>"] = "<C-k>",
+        ["<C-l>"] = "<C-l>",
+        ["<C-;>"] = "<C-;>",
+      },
+    })
+  end,
+}
