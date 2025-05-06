c94330f5acb7b5bd2cea5cc2418d48d7174b6cc0
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2025-05-06 13:04:25 +0300

    feat: install only enable servers

--- lua/config/plugins/mason-lspconfig.lua
+++ lua/config/plugins/mason-lspconfig.lua
@@ -7,9 +7,18 @@ return {
     "neovim/nvim-lspconfig",
   },
   config = function()
-    local mlsp = require("mason-lspconfig")
-    mlsp.setup({
-      ensure_installed = vim.tbl_keys(PREF.lsp.active_servers),
+    local ensure_installed = vim
+      .iter(PREF.lsp.active_servers)
+      :filter(function(_, v)
+        return v
+      end)
+      :fold({}, function(acc, k, v)
+        table.insert(acc, k)
+        return acc
+      end)
+
+    require("mason-lspconfig").setup({
+      ensure_installed = ensure_installed,
       automatic_installation = true,
     })
   end,
