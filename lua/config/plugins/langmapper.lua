c4394bc8cb7dc5f6054fa94e7abff4941a871793
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2025-05-06 13:02:51 +0300

    refactor: made langmapper not lazy

--- lua/config/plugins/langmapper.lua
+++ lua/config/plugins/langmapper.lua
@@ -3,9 +3,9 @@ local DEV = false
 return {
   "Wansmer/langmapper.nvim",
   enabled = true,
+  lazy = false,
   dir = DEV and "~/projects/code/personal/langmapper.nvim" or nil,
   dev = DEV,
-  priority = 1,
   config = function()
     local lm = require("langmapper")
     lm.setup()
