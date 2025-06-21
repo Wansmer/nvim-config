78a573526ee0f9c675c3374ebb6d62b8f60f2d87
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2025-06-21 19:01:52 +0300

    feat: Add setup function and configurable window width for Aider

--- lua/modules/aider/init.lua
+++ lua/modules/aider/init.lua
@@ -5,14 +5,27 @@ function M.new()
   return setmetatable({
     win = nil,
     buf = nil,
+    job = nil,
+    opts = {
+      -- Window width configuration:
+      -- If win_width is a number less than 1 (e.g., 0.4), it's treated as a ratio of the total Neovim columns.
+      -- If win_width is a number 1 or greater (e.g., 80), it's treated as an absolute number of columns.
+      win_width = 0.4,
+    },
   }, M)
+end
+
+-- Setup function to configure the Aider instance
+function M:setup(opts)
+  self.opts = vim.tbl_deep_extend("force", self.opts, opts or {})
 end
 
 function M:open()
   self.buf = self.buf or vim.api.nvim_create_buf(false, false)
+
   self.win = vim.api.nvim_open_win(self.buf, true, {
     vertical = true,
-    width = math.floor(vim.o.columns * 0.4),
+    width = (self.opts.win_width < 1) and math.floor(vim.o.columns * self.opts.win_width) or self.opts.win_width,
   })
 
   self.job = self.job
