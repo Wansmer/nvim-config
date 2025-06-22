82f948a819f28dd1416b4220bbbf7d164959eca5
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2025-06-22 08:34:44 +0300

    refactor: Add option to list only visible buffer files

--- lua/modules/aider/utils.lua
+++ lua/modules/aider/utils.lua
@@ -15,10 +15,24 @@ function M.is_file(file)
   return vim.uv.fs_stat(file) ~= nil
 end
 
---- Returns a list of files associated with the all open buffers.
+--- Returns a list of files associated with the all open buffers. If `visible` is true, it will
+--- return only files associated with visible buffers.
+--- @param visible? boolean
 --- @return string[]
-function M.list_files()
-  return vim.iter(vim.api.nvim_list_bufs()):map(M.get_filepath):filter(M.is_file):totable()
+function M.list_files(visible)
+  local get_bufs = visible and M.list_visible_bufs or vim.api.nvim_list_bufs
+  return vim.iter(get_bufs()):map(M.get_filepath):filter(M.is_file):totable()
+end
+
+--- Returns a list of visible buffers
+--- @return number[]
+function M.list_visible_bufs()
+  return vim
+    .iter(vim.api.nvim_list_wins())
+    :map(function(win)
+      return vim.api.nvim_win_get_buf(win)
+    end)
+    :totable()
 end
 
 return M
