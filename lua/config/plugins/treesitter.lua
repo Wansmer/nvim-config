df0707c05ba03872be26f23c8d076bc9d9285df4
Wansmer (Ivan Smirnov) <wansmer@gmail.com>
2025-09-26 11:06:44 +0300

    feat: add `incr.nvim`

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -4,6 +4,17 @@ return {
   lazy = false,
   build = ":TSUpdate",
   branch = "main",
+  dependencies = {
+    {
+      "daliusd/incr.nvim",
+      config = function()
+        require("incr").setup({
+          incr_key = "<Cr>",
+          decr_key = "<Bs>",
+        })
+      end,
+    },
+  },
   config = function()
     local map = vim.keymap.set
     map("n", "<leader>tp", "<Cmd>InspectTree<Cr>", { nowait = true })
